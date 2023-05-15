eb3aa8a58027fb0572dd9393ceba16f3b7d1fee8
Alexandro Sanchez Bach <alexandro@phi.nz>
2023-05-15 20:09:02 +0200

    Bump to v1.0.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 import codecs
 import setuptools
 
-NTYPES_VERSION = '1.0.6'
+NTYPES_VERSION = '1.0.7'
 NTYPES_REPOSITORY_URL = 'https://github.com/AlexAltea/ntypes'
 NTYPES_DOWNLOAD_URL = 'https://github.com/AlexAltea/ntypes/tarball/' + NTYPES_VERSION
 
